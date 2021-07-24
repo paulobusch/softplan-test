6582921a037d1d167a548a940b84c7abb0f6a8e9
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-24 14:10:51 -0300

    feat: create new project Softplan.UnitTests

--- 2 - Services/Softplan.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Softplan.Services
-{
-    public class Class1
-    {
-    }
-}
---
+++ 2 - Services/Softplan.Services/Fees/FeeService.cs
@@ -0,0 +1,13 @@
+﻿using Softplan.Domain.Interfaces.Services;
+using Softplan.Domain.Params;
+
+namespace Softplan.Services.Fees
+{
+    public class FeeService : IFeeService
+    {
+        public decimal Calculate(FeesParams feesParams)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
