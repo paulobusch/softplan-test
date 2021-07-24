45d4a790becd61e5a329c13927fcc1465ccb349e
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-24 15:55:18 -0300

    feat: create new project Softplan.Api2

---
+++ 1 - Apis/Softplan.Api2/Controllers/SoftplanControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Softplan.Domain.Results;
+
+namespace Softplan.Api2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class SoftplanControllerBase : ControllerBase
+    {
+        protected Result GetResult(Result result)
+        {
+            Response.StatusCode = (int)result.Status;
+            return result;
+        }
+
+        protected Result<T> GetResult<T>(Result<T> result)
+        {
+            Response.StatusCode = (int)result.Status;
+            return result;
+        }
+
+        protected Result<T> GetResult<T>(T data)
+        {
+            return new Result<T>(data);
+        }
+    }
+}
