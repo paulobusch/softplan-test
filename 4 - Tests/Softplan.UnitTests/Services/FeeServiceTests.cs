ccebcfef466c831c655f2a71c1bc4cbc6301dd14
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-24 14:30:13 -0300

    feat: added FeeServiceTests

---
+++ 4 - Tests/Softplan.UnitTests/Services/FeeServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Softplan.Domain.Params;
+using Softplan.Services.Fees;
+using Softplan.UnitTests._Common;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Softplan.UnitTests.Services
+{
+    public class FeeServiceTests : TestBase
+    {
+        public FeeServiceTests(SoftplanFixture _) : base(_) { }
+
+        public static IEnumerable<object[]> CalculateData()
+        {
+            yield return new object[] { new FeesParams { Capital = 100, Time = 5, Fee = 0.01m }, 105.10m };
+            yield return new object[] { new FeesParams { Capital = 100, Time = 5, Fee = 0.08m }, 146.93m };
+            yield return new object[] { new FeesParams { Capital = 100, Time = 12, Fee = 0.01m }, 112.68m };
+            yield return new object[] { new FeesParams { Capital = 50000, Time = 6, Fee = 0.01m }, 52550.50m };
+            yield return new object[] { new FeesParams { Capital = 50000, Time = 6, Fee = 0.08m }, 52550.50m };
+            yield return new object[] { new FeesParams { Capital = 50000, Time = 12, Fee = 0.01m }, 56341.25m };
+        }
+
+        [Theory]
+        [MemberData(nameof(CalculateData))]
+        public void Calculate(FeesParams feesParams, decimal expectedResult)
+        {
+            var service = new FeeService();
+
+            var result = service.Calculate(feesParams);
+
+            Assert.Equal(expectedResult, result, 2);
+        }
+    }
+}
