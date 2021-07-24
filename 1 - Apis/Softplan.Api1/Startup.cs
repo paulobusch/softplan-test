a702883406f5e5fdaed2fcb483829a13ccf354c6
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-24 13:17:46 -0300

    refactor: Softplan.Api1

--- 1 - Apis/Softplan.Api1/Startup.cs
+++ 1 - Apis/Softplan.Api1/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Softplan.Api1
 {
@@ -23,10 +16,8 @@ namespace Softplan.Api1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -34,7 +25,6 @@ namespace Softplan.Api1
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Softplan API's 
 ## Framework
 - NetCore 5.0
+
+## Documentação
+ - https://localhost:44398/swagger/index.html
