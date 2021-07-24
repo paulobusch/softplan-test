faef2479022fc25fdef37c6452322a7f7dbedb47
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-07-24 13:29:07 -0300

    feat: added abstract SoftplanControllerBase

---
+++ 1 - Apis/Softplan.Api1/Controllers/SoftplanControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Softplan.Domain.Core.Results;
+
+namespace Softplan.Api1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class SoftplanControllerBase : ControllerBase
+    {
+        protected Result GetResult(Result result)
+        {
+            Response.StatusCode = (int)result.Status;
+            return result;
+        }
+
+        protected Result<T> GetResult<T>(Result<T> result) where T : class
+        {
+            Response.StatusCode = (int)result.Status;
+            return result;
+        }
+
+        protected Result<T> GetResult<T>(T data) where T : class
+        {
+            return new Result<T>(data);
+        }
+    }
+}
